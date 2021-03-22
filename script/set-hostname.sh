064e08eb66a09b1f3af8100c9adcb973c493048f
KHAN ADIL <36574088+khann-adill@users.noreply.github.com>
2021-03-22 18:58:45 +0530

    Create set-hostname.sh

---
+++ script/set-hostname.sh
@@ -0,0 +1,18 @@
+
+############################################################# PLAY WITH KUBECTL #############################################################################
+#       Setting up hostname as kworker
+#       system compatible: Ubuntu:18.04.5 LTS (Bionic Beaver) and 20.04.1 LTS (Focal Fossa).
+#       Maintainer: Adil Abdullah Khan
+#       To Run this script:
+#       curl  https://raw.githubusercontent.com/adil1806/play-with-kubectl/master/kubernetes-installation/set-hostname-kworker.sh | bash
+#############################################################################################################################################################
+
+#!/bin/bash
+
+echo "[TASK 1] Setting hostname for this node to Kworker"
+hostnamectl set-hostname $1 >/dev/null 2>&1
+
+echo "[TASK 2] Adding newly created hostname to hosts file"
+sed -i 's/^127.0.0.1 .*/127.0.0.1 localhost $1/' hosts >/dev/null 2>&1
+
+echo "[Task 3] Please restart/reboot this system by ourself"
