37de4b60e1e6b686f197257fbf5a2fa1b42e3bda
KHAN ADIL <36574088+khann-adill@users.noreply.github.com>
2021-03-23 18:28:02 +0530

    Update py-ansible.sh

--- py-ansible.sh
+++ py-ansible.sh
@@ -11,9 +11,9 @@ then
 elif [[ $os_name == "Red Hat Enterprise Linux" ]]
 then
         sudo yum update -y >/dev/null 2>&1
-        sudo yum install python3 -y 
-        pip3 install ansible 
-        sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        sudo yum install python3 -y >/dev/null 2>&1
+        pip3 install ansible >/dev/null 2>&1
+        sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm >/dev/null 2>&1
         sudo yum install ansible -y >/dev/null 2>&1
 else
         echo "[❌No Distro is unsupported by this script ]❌"
