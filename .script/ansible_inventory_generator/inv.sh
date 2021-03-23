ef6203480644a4a40bd9e941bb6303409eb743a1
Khan Adil <khanabdullahadil.gmail.com>
2021-03-23 05:40:36 +0000

    remove script folder

---
+++ .script/ansible_inventory_generator/inv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]
+then
+        printf "❌ No hosts provided.\n"
+        exit 45
+else
+        echo "[master]" > /etc/ansible/custom_inv.ini
+        echo $1 ansible_python_interpreter=/usr/bin/python3 >> /etc/ansible/custom_inv.ini
+        echo "[worker]" >> /etc/ansible/custom_inv.ini
+        for node_host in $@
+        do
+                echo $node_host ansible_python_interpreter=/usr/bin/python3 >> /etc/ansible/custom_inv.ini
+        done
+        sed -i '4d' /etc/ansible/custom_inv.ini
+        echo "[kube-cluster:children]" >> /etc/ansible/custom_inv.ini
+        echo "master" >> /etc/ansible/custom_inv.ini
+        echo "worker" >> /etc/ansible/custom_inv.ini
+fi
+printf "✅ Custom inventory file create successfully for setting up k8s cluster.\n"
+echo " "
+printf "❗️If you wish to list the inventory host run below command ❗️\n"
+printf " 🔒 ansible all -i /etc/ansible/custom_inv.ini --list-host 🔒 \n"
