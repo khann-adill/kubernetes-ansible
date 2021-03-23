83abda5afab5db5f9f74d9c89e64559ebefd5fe5
Khan Adil <khanabdullahadil.gmail.com>
2021-03-23 10:33:45 +0000

    code chnage

--- main.sh
+++ main.sh
@@ -8,7 +8,13 @@ then
 	ansible-playbook .addnode.yml --ask-vault-pass
 elif [[ $1 == "reset" ]]
 then
-	ansible-playbook .reset.yml --ask-vault-pass
+	read -p "Do you want to decommission the AWS resources(y/n):" confirm
+	if [[ $confirm == "y" ]]
+	then
+		ansible-playbook .reset.yml --ask-vault-pass
+	else
+		echo "Decommission is cancelled by user"
+	fi
 else
-	echo "Unsupported comannd..! please select from this [initcluster,addnode,reset]"
+	echo "Unsupported comannd..! please select from this [ initcluster , addnode , reset ]"
 fi
