fd2c14fe24db747a920316a6282c6ff708305938
Khan Adil <khanabdullahadil.gmail.com>
2021-03-23 09:42:23 +0000

    added role for reset

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ then
 	ansible-playbook .addnode.yml --ask-vault-pass
 elif [[ $1 == "reset" ]]
 then
-	echo need to add reset role
+	ansible-playbook .reset.yml --ask-vault-pass
 else
 	echo "Unsupported comannd..! please select from this [initcluster,addnode,reset]"
 fi
